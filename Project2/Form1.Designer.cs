9651aaba1b19ec06a3b7832eecd216002dd13d57
dewizt <dewizt@mail.ru>
2020-06-26 12:25:42 +0300

    first commit

---
+++ Project2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(206, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показать данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(206, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(29, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(235, 72);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(186, 199);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(421, 72);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(108, 199);
+            this.listBox3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(421, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(206, 307);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(209, 54);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Выполнить все действия одновременно";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(576, 72);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(412, 199);
+            this.listBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "STATUS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(773, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 433);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Project2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        string[] arrayID = new string[5]; // Массив ID 
+        string[] arrayName = new string[5]; // Массив Имён
+        string[] status = new string[5]; // Массив статустов
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddToMassive();
+            Thread thread = new Thread(ReverseStatus);
+            thread.Start();           
+        }
+        public void AddToMassive()
+        {
+            Thread thread = new Thread(() =>
+            {
+                string connectionStr = "server=localhost;port=3306;user=root;password=root;database=dewizt_db;";
+
+                MySqlConnection conn = new MySqlConnection(connectionStr);
+
+                conn.Open();
+
+                string sql = "SELECT * FROM users";
+
+                MySqlCommand command = new MySqlCommand(sql, conn);
+
+                MySqlDataReader read = command.ExecuteReader();
+
+                int i = 0;
+                while (read.Read() & i < 5)
+                {
+                    arrayID[i] = read[0].ToString();
+                    arrayName[i] = read[1].ToString();
+                    status[i] = read[2].ToString();
+                    i++;
+                }
+                read.Close();
+                conn.Close();
+            });
+            thread.Start();
+            listBox4.Items.Add(DateTime.Now + " добавили ID в массив [ADDTOMASSIVE]");
+            listBox4.Items.Add(DateTime.Now + " добавили NAME в массив [ADDTOMASSIVE]");
+            listBox4.Items.Add(DateTime.Now + " добавили STATUS в массив [ADDTOMASSIVE]");
+            Thread.Sleep(5000);
+        } // Добавление элементов в массивы
+        public void AddToList()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                listBox1.Items.Add(arrayID[i]);
+                listBox2.Items.Add(arrayName[i]);
+                listBox3.Items.Add(status[i]);
+            }
+            listBox4.Items.Add(DateTime.Now + " добавили элементы в массивы");
+        } // Добавление в таблицы
+        public void ReverseStatus()
+        {
+                if (status.Length != 0)
+                {
+                    for (int i = 0; i < status.Length; i++)
+                    {
+                        if (status[i] == "0")
+                        {
+                            status[i] = "1 изменено";
+                            listBox4.Items.Add(DateTime.Now + " произвели замену '0' на " + status[i] + " [REVERSESTATUS]");
+                    }
+                        if (status[i] == "1")
+                        {
+                            status[i] = "0 изменено";
+                            listBox4.Items.Add(DateTime.Now + " произвели замену '1' на " + status[i] + " [REVERSESTATUS]");
+                        }
+                    }
+                }
+                else
+                {
+                    listBox4.Items.Add(DateTime.Now + " НЕЧЕГО ЗАПОЛНЯТЬ");
+                }
+        } // Замена статуса на противоположное значение
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            AddToList();
+        } // Кнопка "Показать данные"
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox4.Items.Add("Удалили ID");
+            listBox2.Items.Clear();
+            listBox4.Items.Add("Удалили NAME");
+            listBox3.Items.Clear();
+            listBox4.Items.Add("Удалили STATUS");
+        } // Кнопка "Удалить"
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AddToList();
+        } // Кнопка "Выполнить всё сразу"
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
